d68f405b02545cf910679a98ec1f6e2f3b5d28eb
szigetif <61885406+szigetif@users.noreply.github.com>
2020-04-05 23:42:39 +0200

    Add files via upload

--- src/hu/bme/mit/brszta/IBoard.java
+++ src/hu/bme/mit/brszta/IBoard.java
@@ -4,7 +4,7 @@ import java.util.List;
 public interface IBoard {
     int getSizeX();
     int getSizeY();
-    List<ICell> getCells();
-    int getNumberOfMines();
+    List<Cell> getCells();
+  //  int getNumberOfMines(); //why would we need that?
     int getNumberOfFlags();
 }
--- src/hu/bme/mit/brszta/Main.java
+++ src/hu/bme/mit/brszta/Main.java
@@ -1,8 +1,20 @@
 package hu.bme.mit.brszta;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Board board_obj = new Board(3, 3, 1); //create board
+
+        List<Cell> all_cells = board_obj.getCells(); //create list of all the cells, only to query their properties
+        for (Cell cell : all_cells) {
+            System.out.println("[mine = " + cell.get_mine() + "] [coordinates= " + cell.get_coordinates() + "] [neighbouring mines= " + cell.reveal() + "]\n");
+        }
+
+
+
     }
 }
