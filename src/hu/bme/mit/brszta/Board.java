848ec3bda4c05d61d7a0bf1c0622c65cda63597f
leventerevesz <levete.revesz@gmail.com>
2020-04-24 15:40:52 +0200

    Remove sizeX, sizeY, numberOfMines variables from Board

--- src/hu/bme/mit/brszta/Board.java
+++ src/hu/bme/mit/brszta/Board.java
@@ -5,28 +5,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class Board{
-
-    private int sizeX;
-    private int sizeY;
-    private int numberOfMines;
     private List<Boolean> mineList;
     private ArrayList<ArrayList<Cell>> cellMatrix;
 
     public Board(int sizeX, int sizeY, int numberOfMines){
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
-        this.numberOfMines = numberOfMines;
-
-        List<Boolean> mineList = createMineList();
-        cellMatrix = createCellMatrix(mineList);
+        List<Boolean> mineList = createMineList(sizeX*sizeY, numberOfMines);
+        cellMatrix = createCellMatrix(sizeX, sizeY, mineList);
 
         initNeighbourCells();
         initDisplayNumbers();
     }
 
-    private List<Boolean> createMineList() {
+    private List<Boolean> createMineList(int len, int numberOfMines) {
         List<Boolean> mineList = new ArrayList<Boolean>();
-        for (int i = 0; i < sizeX*sizeY; i++) {
+        for (int i = 0; i < len; i++) {
             if (i < numberOfMines) {
                 mineList.add(true);
             }
@@ -38,7 +30,7 @@ public class Board{
         return mineList;
     }
 
-    private ArrayList<ArrayList<Cell>> createCellMatrix(List<Boolean> mineList)
+    private ArrayList<ArrayList<Cell>> createCellMatrix(int sizeX, int sizeY, List<Boolean> mineList)
     {
         cellMatrix = new ArrayList<ArrayList<Cell>>();
         for (int row = 0; row < sizeY; row++) {
@@ -54,6 +46,8 @@ public class Board{
 
     private void initNeighbourCells() {
         // Connect the adjacent cells to each cell in the cellMatrix.
+        int sizeX = getSizeX();
+        int sizeY = getSizeY();
         for (int row = 0; row < sizeY; row++) {
             for (int col = 0; col < sizeX; col++) {
                 Cell cell = cellMatrix.get(row).get(col);
@@ -86,15 +80,21 @@ public class Board{
     }
 
     public int getSizeX(){
-        return sizeX;
+        return cellMatrix.get(0).size();
     }
 
     public int getSizeY(){
-        return sizeY;
+        return cellMatrix.size();
     }
 
     public int countMines() {
-        return numberOfMines;
+        int count = 0;
+        for (List<Cell> cellRow : cellMatrix) {
+            for (Cell cell : cellRow) {
+                if (cell.isMine()) count++;
+            }
+        }
+        return count;
     }
 
     public int countFlags() {
@@ -118,7 +118,6 @@ public class Board{
     }
 
     public Cell getCell(int row, int col) {
-        Cell cell = cellMatrix.get(row).get(col);
-        return cell;
+        return cellMatrix.get(row).get(col);
     }
 }
